2a35a29e983236dbc4d1528ccfe64429a5fc3751
Nate <natesteelerfan@gmail.com>
2018-02-20 14:41:54 -0800

    asdf
    asdf

--- TicTac.java
+++ TicTac.java
@@ -7,7 +7,7 @@ import java.util.*;
  */
 public class TicTac
 {
-    private String[][] board = new String[3][3];
+    private String[][] board;
     private Scanner console;
     private int a;
     int size = 0;
@@ -15,7 +15,6 @@ public class TicTac
         console = new Scanner(System.in);
         int u;
         do{
-            reset();
             System.out.println("What do you want to play?");
             System.out.println();
             System.out.println("(0) 2 player");
@@ -27,6 +26,7 @@ public class TicTac
             System.out.print("Choice ----> ");
             System.out.println();
             u = console.nextInt();
+            a = 0;
             if(u == 2){
                 break;
             }
@@ -34,6 +34,8 @@ public class TicTac
             System.out.println();
             System.out.print("n = ");
             size = console.nextInt();
+            board = new String[size][size];
+            reset();
             if(u == 0){
                 System.out.println("Player 1 = X      Player 2 = O");
                 int player = 0;
@@ -114,11 +116,7 @@ public class TicTac
                         System.out.println("Computer:");
                         do{
                             System.out.println();
-                            do{
-                                x = (int)(Math.random() * 3);
-                                y = (int)(Math.random() * 3);
-                            }while(!checkSpot(x , y));
-                            move(a , x , y);
+                            findMove(player);
                             a--;
                         }while(a == 1);
                         
@@ -131,7 +129,7 @@ public class TicTac
     public void reset(){
         for(int i = 0; i < board.length; i++){
             for (int j = 0; j < board[0].length; j++){
-                board[i][j] = "open";
+                board[i][j] = "-";
             }
         }
     }
@@ -139,18 +137,24 @@ public class TicTac
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println("  0       1       2");
+        for(int i = 0 ; i < board.length; i++){
+            System.out.print("-----");
+        }
+        System.out.println();
         for(int i = 0; i < board.length; i++){
-            System.out.print(i + " ");
             for (int j = 0; j < board[0].length; j++){
-                System.out.print(board[i][j] + "    ");
+                System.out.print(" [" + board[i][j] + "] ");
             }
             System.out.println();
+        }
+        for(int i = 0 ; i < board.length; i++){
+            System.out.print("-----");
         }
         System.out.println();
         System.out.println();
         System.out.println();
     }
+
     public boolean check(int a){
         if(a == 0){
             a++;
@@ -158,32 +162,48 @@ public class TicTac
         else{
             a--;
         }
-        for(int i = 0; i < 3; i++){
-            if(board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2]) && !board[i][0].equals("open")){
-                System.out.println("Player " + getMove1(a) + " Wins!!!");
-                System.out.println();
-                System.out.println();
+        for(int i = 0; i < board.length; i++){
+            boolean b = true;
+            for(int j = 0; j < board.length && b; j++){
+                if(!board[i][j].equals(getMove(a))){
+                     b = false;
+                }
+            }
+            if(b){
+                System.out.println("Player " + getMove1(a) + " Wins!!!!");
                 return true;
             }
         }
-        for(int i = 0; i < 3; i++){
-            if(board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i]) && !board[0][i].equals("open")){
-                System.out.println("Player " + getMove1(a) + " Wins!!!");
-                System.out.println();
-                System.out.println();
+        for(int i = 0; i < board.length; i++){
+            boolean b = true;
+            for(int j = 0; j < board.length && b; j++){
+                if(!board[j][i].equals(getMove(a))){
+                     b = false;
+                }
+            }
+            if(b){
+                System.out.println("Player " + getMove1(a) + " Wins!!!!");
                 return true;
             }
         }
-        if(board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2]) && !board[0][0].equals("open")){
-            System.out.println("Player " + getMove1(a) + " Wins!!!");
-            System.out.println();
-            System.out.println();
+        boolean b = true;
+        for(int i = 0; i < board.length; i++){
+            if(!board[i][i].equals(getMove(a))){
+                     b = false;
+            }
+        }
+        if(b){
+            System.out.println("Player " + getMove1(a) + " Wins!!!!");
             return true;
         }
-        if(board[2][0].equals(board[1][1]) && board[2][0].equals(board[0][2]) && !board[0][2].equals("open")){
-            System.out.println("Player " + getMove1(a) + " Wins!!!");
-            System.out.println();
-            System.out.println();
+        b = true;
+        for(int i = 0; i < board.length; i++){
+            if(!board[i][board.length - 1 - i].equals(getMove(a))){
+                     b = false;
+            }
+        }
+        if(b){
+            System.out.println("Player " + getMove1(a) + " Wins!!!!");
             return true;
         }
         if(cats()){
@@ -195,14 +215,14 @@ public class TicTac
         return false;
     }
     public void move(int player ,int x , int y){
-        if(board[y][x].equals("open")){
+        if(board[y][x].equals("-")){
             board[y][x] = getMove(player);
         }
     }
     public boolean cats(){
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(board[i][j].equals("open")){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j].equals("-")){
                     return false;
                 }
             }
@@ -211,10 +231,10 @@ public class TicTac
     }
     public String getMove(int x){
         if(x == 0){
-            return "X   ";
+            return "X";
         }
         else{
-            return "O   ";
+            return "O";
         }
     }
     public String getMove1(int x){
@@ -229,10 +249,74 @@ public class TicTac
         if(x < 0 && x > 2 && y < 0 && y > 2){
             return false;
         }
-        if(board[y][x].equals("open")){
+        if(board[y][x].equals("-")){
             return true;
         }
         return false;
+    }
+    public void findMove(int player){
+        for(int i = 0; i < board.length; i ++){
+            int a = 0;
+            for(int j = 0; j < board[0].length; j++){
+                if(!board[i][j].equals("X")){
+                    a++;
+                }
+            }
+            if(a == 1){
+                for(int j = 0; j < board[0].length; j++){
+                    if(board[i][j].equals("-")){
+                        move(i , j , player);
+                        return;
+                    }
+                }
+            }
+        }
+        for(int j = 0; j < board.length; j ++){
+            int a = 0;
+            for(int i = 0; i < board[0].length; i++){
+                if(!board[i][j].equals("X")){
+                    a++;
+                }
+            }
+            if(a == 1){
+                for(int i = 0; i < board[0].length; i++){
+                    if(board[i][j].equals("-")){
+                        move(i , j , player);
+                        return;
+                    }
+                }
+            }
+        }
+        int i;
+        int a = 0;
+        for(i = 0; i < board.length; i++){
+            if(!board[i][i].equals("X")){
+                a++;
+            }
+        }
+        if (a == 1){
+            for(int j = 0; j < board.length; j++){
+                if(board[j][j].equals("-")){
+                    move(i , i , player);
+                    return;
+                }
+            }
+        }
+        a = 0;
+        for(i = 0; i < board.length; i++){
+            if(!board[i][board.length - 1 - i].equals("X")){
+                a++;
+            }
+        }
+        if (a == 1){
+            for(int j = 0; j < board.length; j++){
+                if(board[j][board.length - 1 - j].equals("-")){
+                    move(i , i , player);
+                    return;
+                }
+            }
+        }
+        move((int)(Math.random() * size) , (int)(Math.random() * size) , player);
     }
 }
 
